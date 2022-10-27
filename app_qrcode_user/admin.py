9635bc5b2d1b86a7b6e3666e37e08fc7e738c87e
Achille kabasele <pepexykabasele@gmail.com>
2022-10-27 10:07:11 +0200

    add form file

--- app_qrcode_user/admin.py
+++ app_qrcode_user/admin.py
@@ -5,11 +5,12 @@ from django.utils.html import format_html
 # Register your models here.
 
 # Model Admin room and booking
-@admin.register(CustomerUser)
+@admin.register(ClientUser)
 class CLientUserAdmin(admin.ModelAdmin):
     def image_tag(self, obj):
         return format_html('<img src="{}" width="100" height="50" />'.format(obj.profile_image.url))
 
     image_tag.short_description = 'Profile Image'
-    list_display = ('name','email','image_tag')
+    #list_display = ('name','email','image_tag')
+    list_display = ['admin']
 
--- parameter/urls.py
+++ parameter/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app_qrcode_user.urls'), name='qrcode__user')
+    path('', include('app_qrcode_user.urls'), name='qrcode__user'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
