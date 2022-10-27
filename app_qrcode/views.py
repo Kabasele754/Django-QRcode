2a93a85e4a75cc10adfd6f4243867ecdaa18bedb
Achille kabasele <pepexykabasele@gmail.com>
2022-10-27 13:09:30 +0200

    update template

--- app_qrcode/views.py
+++ app_qrcode/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import MyQrCode
 
 # Create your views here.
 
 
 class HpmeQRCode(TemplateView):
     template_name= "home_qrcode.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super(HpmeQRCode, self).get_context_data(**kwargs)
+        context['my_qrcode'] = MyQrCode.objects.all()
+        
+        return context
