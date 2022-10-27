87e2072c72e5c43ea92ab7998acbd6300719f78c
Achille kabasele <pepexykabasele@gmail.com>
2022-10-27 11:37:50 +0200

    update

--- app_qrcode/models.py
+++ app_qrcode/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+import qrcode
+from io import BytesIO
+from django.core.files import File
+from PIL import Image, ImageDraw
 
 # Create your models here.
 
@@ -6,3 +10,4 @@ from django.db import models
 
 class MyQrCode(models.Model):
     name = models.CharField(max_length=40,)
+    qr_code = models.ImageField(upload_to='qrcode/')
--- parameter/settings.py
+++ parameter/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # add my user qrcode
-    'app_qrcode_user.apps.AppQrcodeUserConfig',
+    #'app_qrcode_user.apps.AppQrcodeUserConfig',
     'app_qrcode.apps.AppQrcodeConfig',
 ]
 
@@ -123,8 +123,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles")
 MEDIA_ROOT = os.path.join(BASE_DIR/ 'media')
 
-AUTH_USER_MODEL = 'app_qrcode_user.CustomerUser'
-ADMIN_MEDIA_PREFIX = '/media/'
+#AUTH_USER_MODEL = 'app_qrcode_user.CustomerUser'
+#ADMIN_MEDIA_PREFIX = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
